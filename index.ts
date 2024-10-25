1ada5dfc43130b0b5d44b6bdfa146396781d5fe1
previousdolphin <previousdolphin@users.noreply.github.com>
2024-10-24 22:52:08 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std@0.106.0/http/server.ts";
+
+const port = parseInt(Deno.env.get("PORT") || "8000");
+const server = serve({ hostname: "0.0.0.0", port });
+
+console.log(`Server is running on http://0.0.0.0:${port}`);
+
+for await (const req of server) {
+  req.respond({ body: "Hello, world!\n" });
+}
